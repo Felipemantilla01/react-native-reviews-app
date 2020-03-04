cb7da4855ab774d5d0155f5bbefdd0dddb40e4e7
Felipe mantilla <felipemantilla@outlook.es>
2020-03-04 16:22:48 -0500

    [feat] using images for rating

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,18 +1,22 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { globalStyles } from '../styles/global'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import { globalStyles, images } from '../styles/global'
 import { Button } from 'react-native-elements';
 import Card from '../shared/card'
 
 const ReviewDetails = ({navigation}) => {
 
+    const rating = navigation.getParam('rating')
     
     return (
         <View style={globalStyles.container}>
             <Card>
-            <Text>{navigation.getParam('title')}</Text>
-            <Text>{navigation.getParam('body')}</Text>
-            <Text>{navigation.getParam('rating')}</Text>
+                <Text>{navigation.getParam('title')}</Text>
+                <Text>{navigation.getParam('body')}</Text>
+                <View style={styles.rating}>
+                    <Text>GameZone rating:</Text>
+                    <Image source={images.ratings[rating]} />
+                </View>
             </Card>
         </View>
     )
@@ -20,6 +24,14 @@ const ReviewDetails = ({navigation}) => {
 
 const styles = StyleSheet.create({
 
+    rating:{
+        flexDirection:'row',
+        justifyContent:'center',
+        paddingTop:16,
+        marginTop:16,
+        borderTopWidth:1,
+        borderTopColor:'#eee'
+    }
 })
 
 
